0ff92f171a648e4dfde77613c8490648b6a43b77
David Airisniemi <David.Airisniemi@hotmail.com>
2022-10-13 16:09:34 +0200

    Add project files.

---
+++ IoTAdminApp/MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTAdminApp.MVVM.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private object _currentView;
+
+        public RelayCommand BedroomViewCommand { get; set; }
+
+
+        public BedroomViewModel BedroomViewModel { get; set; }
+
+
+
+        public MainWindowViewModel()
+        {
+
+            BedroomViewModel = new BedroomViewModel();
+
+
+
+            BedroomViewCommand = new RelayCommand(x => { CurrentView = BedroomViewModel; });
+
+
+            CurrentView = BedroomViewModel;
+        }
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
